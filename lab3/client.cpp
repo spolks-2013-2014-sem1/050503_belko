c0d3ff7698e21a64b56bc4827da254af5f50d234
sevsoad <sevsoad@ya.ru>
2013-11-21 11:02:02 +0300

    bugs fixed

--- lab3/client.cpp
+++ lab3/client.cpp
@@ -5,56 +5,81 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <fstream>
+#include <unistd.h>
 
 using namespace std;
 
+int showErrorMessage(const char* message){
+  cout << message;
+  return 0;
+}
+
+
+
 int main(int argc,char *argv[]) {
+
   int client_descriptor;
   struct sockaddr_in client_address;
-  const int kBufferSize = 128;
+  const int kBufferSize = 8;
   char buffer[kBufferSize];
-  int n;
+  int temp = 0;
+  long file_size = 0;
   ofstream file_stream;
 
   if (argc < 2) {
-    cout<<"There is no filename of file to recieve.\n";
-    return 0;
+    return showErrorMessage("Error: no path to save file.\n");
   }
+
   file_stream.open(argv[1],ios::binary);
+
   client_descriptor = socket(AF_INET,SOCK_STREAM, 0);
   if (client_descriptor == -1) {
-    cout<<("Unable to create socket.\n");
-    return 0;
+    return showErrorMessage("Unable to create socket.\n");
   }
-  memset(&client_address, 0, sizeof(client_address));
+
+  memset (&client_address, 0, sizeof(client_address));
   client_address.sin_family = AF_INET;
   client_address.sin_port = htons(3333);
   if (inet_pton(AF_INET, "127.0.0.1", &client_address.sin_addr) < 0) {
-    cout<<"inet_pton error\n";
+    return showErrorMessage("inet_pton error\n");
   }
+
+  cout << "looking for server\n";
   while(1) {
-    if (connect(client_descriptor, (const sockaddr*) &client_address,
-                 sizeof(client_address)) == -1) {
+    if (connect (client_descriptor, (const sockaddr*) &client_address,
+                 sizeof (client_address)) == -1) {
       continue;
+
     } else {
-        cout<<"connected\n";
-        memset(buffer,0,kBufferSize);
-        while(1) {
-          n = recv(client_descriptor,buffer,kBufferSize,0);
-          if (n == -1){
-            cout<<"Error while getting file.\n";
-            break;
-          }
-          if (n == 0){
-            cout<<"File recieved.\n";
-            break;
-         }
-         file_stream << buffer;
+        cout << "connected\n";
+        memset (buffer,0,kBufferSize);
+
+        recv (client_descriptor,buffer,kBufferSize,0);
+
+        file_size = strtol (buffer, NULL, 10);
+        cout << file_size;
+        send (client_descriptor, buffer, kBufferSize,0);
+        cout << "recieving started\n";
+
+        while  (temp < file_size) {
+
+          memset (buffer,0,kBufferSize);
+          recv (client_descriptor, buffer, kBufferSize,0);
+          file_stream << buffer;
+          send (client_descriptor, buffer, kBufferSize,0);
+          temp += kBufferSize;
+          cout << "\n";
+          cout << temp;
         }
       }
+
     break;
   }
+
+  cout << "file saved.\n";
   file_stream.close();
+  close(client_descriptor);
   return 0;
 }
--- lab3/server.cpp
+++ lab3/server.cpp
@@ -4,53 +4,83 @@
 #include <string.h>
 #include <netinet/in.h>
 #include <fstream>
+#include <unistd.h>
+#include <stdlib.h>
 
 using namespace std;
 
+int showErrorMessage (const char* message){
+
+  cout << message;
+  return 0;
+}
+
+
 int main(int argc,char *argv[]) {
+
   int server_descriptor, client_descriptor;
   struct sockaddr_in server_address;
-  char *memory_block;
+  const int kBufferSize = 8;
   int file_size;
+  char buffer[kBufferSize];
 
   if (argc < 2) {
-    cout<<"There is no filename of file to send.\n";
-    return 0;
+    return showErrorMessage ("Error: no path to save file.\n");
   }
-  ifstream file_stream(argv[1], ios::in|ios::ate|ios::binary);
-  server_descriptor = socket(AF_INET,SOCK_STREAM, 0);
+
+  ifstream file_stream (argv[1], ios::in|ios::ate|ios::binary);
+  if (!file_stream.is_open()){
+    return showErrorMessage ("Can't open file.\n");
+  }
+
+  int yes=1;
+  server_descriptor = socket (AF_INET, SOCK_STREAM, 0);
   if (server_descriptor == -1) {
-    cout<<("Unable to create socket.\n");
-    return 0;
+    return showErrorMessage ("Unable to create socket.\n");
   }
-  memset(&server_address, 0, file_sizeof(server_address));
+
+  if (setsockopt(server_descriptor, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
+    showErrorMessage ("setsockopt");
+  }
+
+  memset (&server_address, 0, sizeof(server_address));
   server_address.sin_family = AF_INET;
   server_address.sin_port = htons(3333);
   server_address.sin_addr.s_addr = INADDR_ANY;
-  if (bind(server_descriptor,(sockaddr *) &server_address,file_sizeof(server_address)) == -1) {
-    cout<<("Binding error.\n");
-    return 0;
+
+  if (bind (server_descriptor,(sockaddr *) &server_address, sizeof(server_address)) == -1) {
+    return showErrorMessage ("Binding error.\n");
   }
+
   listen(server_descriptor,1);
-  cout<<"Server started\n";
-  client_descriptor = accept(server_descriptor,NULL,NULL);
+  cout << "Server started\n";
+
+  client_descriptor = accept (server_descriptor,NULL,NULL);
   if (client_descriptor == -1) {
-    cout<<("Accept error.\n");
-    close(server_descriptor);
-    return 0;
+    close (server_descriptor);
+    return showErrorMessage ("Accept error.\n");
   }
-  if (file_stream.is_open())  {
-    file_size = file_stream.tellg();
-    memory_block = new char [file_size];
-    file_stream.seekg (0, ios::beg);
-    file_stream.read (memory_block, file_size);
-    file_stream.close();
-    write(client_descriptor,memory_block,file_size); //here
-    cout << "File copied.\n";
-    delete[] memory_block;
-  } else {
-    cout<<"unable to open file\n";
+  cout << "client connected.\n";
+
+  file_size = file_stream.tellg();
+  file_stream.seekg (0, ios::beg);
+  sprintf (buffer,"%i",file_size);
+  send (client_descriptor, buffer, kBufferSize,0);
+  recv (client_descriptor, buffer, kBufferSize,0);
+
+  cout << "sending started.\n";
+  while (file_stream.tellg() != -1) {
+
+    memset (buffer,0,kBufferSize);
+    file_stream.read (buffer, kBufferSize);
+    send (client_descriptor,buffer,kBufferSize,0);
+    send (client_descriptor,buffer,kBufferSize,0);
+    memset (buffer,0,kBufferSize);
+
+    recv (client_descriptor, buffer, kBufferSize,0);
   }
+
+  file_stream.close();
   close(client_descriptor);
   close(server_descriptor);
   return 0;
